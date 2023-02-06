aac33311545dd89bc3ce2ec246a14822cc8862cc
LeeChangHwan <changhwanlee94@gmail.com>
2023-02-06 21:05:32 +0900

    라우터 설정, SearchHeader 스타일링

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import SearchHeader from './components/SearchHeader';
+function App() {
+    return (
+        <div>
+            <SearchHeader />
+            <Outlet />
+        </div>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Error from './pages/Error';
+import VideoDetail from './pages/VideoDetail';
+import Videos from './pages/Videos';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <Error />,
+        children: [
+            {
+                index: true,
+                element: <Videos />,
+            },
+            {
+                path: 'videos',
+                element: <Videos />,
+            },
+            {
+                path: 'videos/:keyword',
+                element: <Videos />,
+            },
+            {
+                path: 'videos/watch/:videoId',
+                element: <VideoDetail />,
+            },
+        ],
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>,
 );

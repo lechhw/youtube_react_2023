f8e8a738afc32595937aeb603fa8cb4b422c6e2a
LeeChangHwan <changhwanlee94@gmail.com>
2023-02-06 15:20:17 +0900

    중간커밋

---
+++ src/pages/Error.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Error() {
+    return <p>Not Found</p>;
+}
+
+export default Error;
---
+++ src/pages/VideoDetail.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function VideoDetail() {
+    return <div>Video Detail</div>;
+}
+
+export default VideoDetail;
